82a2ce736635689a8209e5731f1d45c0fc20406d
Artem Sivyankov <artemsivyankov@coherentsolutions.com>
2021-09-29 18:02:15 +0300

    feat: added users card and active button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,15 @@ import { AppComponent } from './app.component';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { ThreePageComponent } from './three-page/three-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [AppComponent, FirstPageComponent, SecondPageComponent, ThreePageComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule, MatButtonModule],
+    // exports: [MatCardModule, MatButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./first-page.component.scss'],
 })
 export class FirstPageComponent implements OnInit {
+    public users = [
+        { name: 'Andrey', age: 24, activated: true },
+        { name: 'Igor', age: 18, activated: true },
+        { name: 'Misha', age: 25, activated: true },
+        { name: 'Marina', age: 30, activated: true },
+        { name: 'Vasya', age: 40, activated: true },
+    ];
+
+    public hiddenUsers = true;
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    isActiveUser(user: { name: string; age: number; activated: boolean }) {
+        this.users.find((obj) => {
+            if (obj.name === user.name) {
+                obj.activated = !obj.activated;
+            }
+        });
+    }
+
+    toogleHideUsers() {
+        this.hiddenUsers = !this.hiddenUsers;
+    }
 }

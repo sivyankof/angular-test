8f9b11b99fa386810d7a5473485eafa794578e17
Artem Sivyankov <artemsivyankov@coherentsolutions.com>
2021-11-13 16:51:15 +0300

    feat: edit server

--- src/app/shared-module/service/users.service.ts
+++ src/app/shared-module/service/users.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { delay } from 'rxjs/operators';
 
 import { User } from 'src/app/shared-module/interface/user.interface';
 import { users } from '../data/users';
@@ -11,7 +11,7 @@ import { users } from '../data/users';
 })
 export class UsersService {
     private URL = 'https://api.github.com';
-    private TOKEN = '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';
+    // private TOKEN = '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';
     private user: BehaviorSubject<User> = new BehaviorSubject<User>(null);
     public userObs: Observable<User> = this.user.asObservable();
 
@@ -23,20 +23,10 @@ export class UsersService {
     }
 
     getUsers(countPage): Observable<any> {
-        // this.http
-        //     .get('https://api.github.com/search/users', {
-        //         headers: {
-        //             Accept: `application/vnd.github.v3+json`,
-        //             authorization: `token ${this.TOKEN}`,
-        //         },
-        //         params: { q: 'q', per_page: 50, page: 0, order: 'desc', sort: 'created_at' },
-        //     })
-        //     .subscribe((x) => console.log(x));
-
         return this.http.get(this.URL + '/users', {
             headers: {
                 Accept: `application/vnd.github.v3+json`,
-                authorization: `token ${this.TOKEN}`,
+                // authorization: `token ${this.TOKEN}`,
             },
             params: { per_page: countPage * 10 },
         });
@@ -50,7 +40,7 @@ export class UsersService {
         return this.http.get(this.URL + `/users/${id}`, {
             headers: {
                 Accept: `application/vnd.github.v3+json`,
-                authorization: `token ${this.TOKEN}`,
+                // authorization: `token ${this.TOKEN}`,
             },
         });
     }

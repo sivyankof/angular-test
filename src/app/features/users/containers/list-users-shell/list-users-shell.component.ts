40bd0baa58a21a3040f2876ad720604b23606de0
Artem Sivyankov <artemsivyankov@coherentsolutions.com>
2021-11-30 18:29:06 +0300

    fix

--- src/app/features/users/containers/list-users-shell/list-users-shell.component.ts
+++ src/app/features/users/containers/list-users-shell/list-users-shell.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { User } from 'src/app/shared-module/interface/user.interface';
-import { UsersService } from 'src/app/shared-module/service/users.service';
 import { loadNewUsers, loadUsers, setActivateUser, userNonActivate } from '../../store/user.actions';
 import { selectUsers } from '../../store/user.selector';
 import { IUserState } from '../../store/user.reducers';
@@ -12,14 +10,12 @@ import { IUserState } from '../../store/user.reducers';
     selector: 'Users',
     templateUrl: './list-users-shell.component.html',
     styleUrls: ['./list-users-shell.component.scss'],
-    providers: [UsersService],
 })
-export class ListUsersComponent implements OnInit, OnDestroy {
+export class ListUsersComponent implements OnInit {
     public hiddenUsers: boolean = true;
     public users$ = this.store.select(selectUsers);
-    private destroy$: Subject<void> = new Subject();
 
-    constructor(public usersService: UsersService, private store: Store<IUserState>) {}
+    constructor(private store: Store<IUserState>) {}
 
     ngOnInit(): void {
         this.store.dispatch(loadUsers());
@@ -35,10 +31,5 @@ export class ListUsersComponent implements OnInit, OnDestroy {
     setActiveAllUsers() {
         this.store.dispatch(setActivateUser());
         this.users$ = this.store.select(selectUsers);
-    }
-
-    ngOnDestroy() {
-        this.destroy$.next();
-        this.destroy$.complete();
     }
 }
